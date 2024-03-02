c240caab91478b70574a71f01daabf12e93ec5c4
Ruddy Setiadi Gunawan <radi30sg@outlook.com>
2024-03-02 16:47:16 -0500

    Simplify DB connection

--- execute_scripts.sh
+++ execute_scripts.sh
@@ -17,7 +17,7 @@ echo "Running Python scripts..."
 "$PYTHON_PATH" bnb_script.py
 
 # Run SQL script
-echo "Running SQL scripts..."
-"$PSQL_PATH" -v ON_ERROR_STOP=1 -U "$PGUSER" -d "$PGDATABASE" -f alter_data_types.sql
+echo "Running SQL script..."
+"$PSQL_PATH" -v ON_ERROR_STOP=1 -U "$DB_USER" -d "$DB_NAME -f alter_data_types.sql
 
 echo "All scripts executed successfully."
